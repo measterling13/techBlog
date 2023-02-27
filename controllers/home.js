bc69f3fa122bdf37597a20241b8ce59ebf67179f
Tigro <easterlingme95@gmail.com>
2023-02-27 14:04:43 -0600

    added login route

--- controllers/home.js
+++ controllers/home.js
@@ -91,3 +91,12 @@ router.get('/post/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
