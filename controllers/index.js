25cc1f01baafde1481b9b095e9d90e5343659d96
Tigro <easterlingme95@gmail.com>
2023-02-27 17:55:59 -0600

    fixing route order

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home.js');
 const dashRoutes = require('./dashboard.js');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashRoutes);
-router.use('/', homeRoutes);
 
 module.exports = router;
