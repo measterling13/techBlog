dd54ac64ccec50bd382d493012e4e731c566df89
Tigro <easterlingme95@gmail.com>
2023-02-27 14:37:56 -0600

    added the findall comments route

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -5,3 +5,12 @@ const {
     Comment
 } = require('../../model');
 const withAuth = require('../../utils/auth');
+
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
