969d8033dbcb9c9e0ed27088c4860d0851ac3cfd
Tigro <easterlingme95@gmail.com>
2023-02-27 14:18:47 -0600

    added the find all users route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -4,3 +4,16 @@ const {
     Post,
     Comment
 } = require('../../models');
+
+router.get('/', (req, res) => {
+    User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
