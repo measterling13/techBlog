bd08bc1b738eb288eeae003cf6cd33691fb1ad01
Tigro <easterlingme95@gmail.com>
2023-02-27 15:33:21 -0600

    added the edit post function

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -38,25 +38,54 @@ async function newFormHandler(event) {
 }
 
 async function createPostHandler(event) {
-    event.preventDefault();
-  
-    document.location.replace("/dashboard/new");
+  event.preventDefault();
+
+  document.location.replace("/dashboard/new");
+}
+
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${post_id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
   }
-  
-  async function deleteFormHandler(event) {
-    event.preventDefault();
-  
-    const post_id = window.location.toString().split("/")[
-      window.location.toString().split("/").length - 1
-    ];
-  
-    const response = await fetch(`/api/posts/${post_id}`, {
-      method: "DELETE",
-    });
-  
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
+}
+
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${post_id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
   }
+}
