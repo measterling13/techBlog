87c5b91dd58c866b214adada70fe50f8c5f07010
Tigro <easterlingme95@gmail.com>
2023-02-27 14:48:08 -0600

    added comment relations

--- model/index.js
+++ model/index.js
@@ -17,3 +17,18 @@ Post.belongsTo(User, {
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 })
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
