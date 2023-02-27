e20a2e701d4385564c14b75c1b9033c6c5cfb2ee
Tigro <easterlingme95@gmail.com>
2023-02-27 14:30:34 -0600

    added the find all post route within api

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -5,3 +5,30 @@ const {
     Comment
 } = require('../../model');
 const withAuth = require('../../utils/auth');
+
+router.get("/", (req, res) => {
+    Post.findAll({
+            attributes: ["id", "content", "title", "created_at"],
+            order: [
+                ["created_at", "DESC"]
+            ],
+            include: [{
+                    model: User,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+            ],
+        })
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
