242ec1cd5aaf6b03d2a93816d23401918dac5f85
Tigro <easterlingme95@gmail.com>
2023-02-27 14:46:17 -0600

    added the relations between user having post and comments

--- model/index.js
+++ model/index.js
@@ -1,3 +1,11 @@
 const User = require('./user');
 const Post = require('./posts');
 const Comment = require('./comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
