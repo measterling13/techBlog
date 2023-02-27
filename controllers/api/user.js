4eca80a8e7349dd8f66f6ba7406478d0499c7cad
Tigro <easterlingme95@gmail.com>
2023-02-27 14:22:09 -0600

    added the finding one user

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -17,3 +17,40 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                    model: Post,
+                    attributes: ['id', 'title', 'content', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ]
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with this id'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
