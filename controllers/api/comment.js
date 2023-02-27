3012d2a1ca476e665afff19b388c7934071282ea
Tigro <easterlingme95@gmail.com>
2023-02-27 14:38:58 -0600

    added the comment creation route

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -14,3 +14,21 @@ router.get("/", (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
+
+
+module.exports = router;
