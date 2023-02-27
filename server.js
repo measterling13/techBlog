70ebcca671ae39f4a7eb904aca4525d6d90bf356
Tigro <easterlingme95@gmail.com>
2023-02-27 13:30:02 -0600

    added route connections and engine creation

--- server.js
+++ server.js
@@ -24,3 +24,17 @@ const sess = {
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
