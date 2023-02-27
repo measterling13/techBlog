97dd8b192c8880a539101bdb985d22bb0d1f7a0b
Tigro <easterlingme95@gmail.com>
2023-02-27 15:31:37 -0600

    added creating post and deleting post functions

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -36,3 +36,27 @@ async function newFormHandler(event) {
     alert(response.statusText);
   }
 }
+
+async function createPostHandler(event) {
+    event.preventDefault();
+  
+    document.location.replace("/dashboard/new");
+  }
+  
+  async function deleteFormHandler(event) {
+    event.preventDefault();
+  
+    const post_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+  
+    const response = await fetch(`/api/posts/${post_id}`, {
+      method: "DELETE",
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
